b9504178ece01e5e490add4e858a7023d38b74ce
Christian Montero Martínez <christianmontero08@gmail.com>
2020-06-09 17:33:02 -0700

    Add logo to header, change favicon and remove manifest

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -2,6 +2,9 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import { makeStyles } from '@material-ui/styles';
+
+import logo from '../../assets/logo.svg'
 
 function ElevationScroll(props) {
     const { children } = props;
@@ -16,12 +19,28 @@ function ElevationScroll(props) {
     });
 }
 
+const useStyles = makeStyles(theme => ({
+    toolbarMargin: {
+        ...theme.mixins.toolbar,
+        marginBottom: '3em'
+    },
+    logo: {
+        height: '7em'
+    }
+}))
+
 export default function Header(){
+    const classes = useStyles();
     return(
-        <ElevationScroll>
-            <AppBar>
-                <Toolbar>Arc Development</Toolbar>
-            </AppBar>
-        </ElevationScroll>
+        <React.Fragment>
+            <ElevationScroll>
+                <AppBar>
+                    <Toolbar disableGutters>
+                        <img alt='company_logo' className={classes.logo}  src={logo}/>
+                    </Toolbar>
+                </AppBar>
+            </ElevationScroll>
+            <div className={classes.toolbarMargin}/>
+        </React.Fragment>
     )
 }
--- src/components/ui/Theme.js
+++ src/components/ui/Theme.js
@@ -1,5 +1,23 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 
+
+const arcBlue = "#0B72B9";
+const arcOrange = "#FFBA60";
+
 export default createMuiTheme({
-    
+    palette: {
+        common: {
+            arcBlue: `${arcBlue}`,
+            arcOrange: `${arcOrange}`
+        },
+        primary:{
+            main: `${arcBlue}`
+        }, secondary: {
+            main: `${arcOrange}`,
+        }
+    },typography: {
+        h3: {
+            fontWeight: 300
+        }
+    }
 });
