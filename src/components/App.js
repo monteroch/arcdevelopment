a4f1dbb7ecef7753ea99a2583e4b6a0604db798e
Christian Montero Martínez <christianmontero08@gmail.com>
2020-06-10 16:30:45 -0700

    Implement drop down menu on header

--- src/components/App.js
+++ src/components/App.js
@@ -14,8 +14,9 @@ function App() {
           <Route exact path="/services" component={() => <div>Services</div>}/>
           <Route exact path="/customsoftware" component={() => <div>Custom software</div>}/>
           <Route exact path="/mobileapps" component={() => <div>Mobile apps</div>}/>
-          <Route exact path="/aboutus" component={() => <div>About us</div>}/>
+          <Route exact path="/websites" component={() => <div>Websites</div>}/>
           <Route exact path="/revolution" component={() => <div>Revolution</div>}/>
+          <Route exact path="/aboutus" component={() => <div>About us</div>}/>
           <Route exact path="/contact" component={() => <div>Contact us</div>}/>
           <Route exact path="/estimate" component={() => <div>estimate</div>}/>
         </Switch>
--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -7,6 +7,9 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+
 
 import logo from '../../assets/logo.svg'
 
@@ -51,31 +54,100 @@ const useStyles = makeStyles(theme => ({
         "&:hover": {
             backgroundColor: "transparent"
         }
+    },
+    menu: {
+        backgroundColor: theme.palette.common.arcBlue,
+        color: 'white',
+        borderRadius: '0px'
+    },
+    menuItem: {
+        ...theme.typography.tab,
+        opacity: 0.7,
+        "&:hover": {
+            opacity: 1
+        }
     }
 }))
 
 export default function Header(){
     const classes = useStyles();
     const [value, setValue] = useState(0);
+    const [anchorEl, setanchorEl] = useState(null);
+    const [open, setOpen] = useState(false);
+    const [selectedIndex, setSelectedIndex] = useState(0);
 
     const handlechange = (e, value) => {
         setValue(value)
     }
 
+    const handleClick = (e) => {
+        setanchorEl(e.currentTarget);
+        setOpen(true);
+    }
+
+    const handleMenuItemClick = (event, index) => {
+        setanchorEl(null);
+        setOpen(false);
+        setSelectedIndex(index);
+    };
+
+    const handleClose = (e) => {
+        setanchorEl(null);
+        setOpen(false);
+    }
+
+    const menuOptions = [
+        {name: 'Services', link: '/services'},
+        {name: 'Custome Software Development', link: '/customsoftware'},
+        {name: 'Mobile App Development', link: '/mobileapps'},
+        {name: 'Websites Development', link: '/websites'},
+    ];
+
     useEffect(() => {
-        if(window.location.pathname === "/" && value !== 0)
-            setValue(0)
-        else if (window.location.pathname === "/services" && value !== 1)
-            setValue(1);
-        else if (window.location.pathname === "/revolution" && value !== 2)
-            setValue(2);
-        else if (window.location.pathname === "/aboutus" && value !== 3)
-            setValue(3);
-        else if (window.location.pathname === "/contact" && value !== 4)
-            setValue(4);
-        else if (window.location.pathname === "/estimate" && value !== 5)
-            setValue(5);
-    })
+        switch(window.location.pathname){
+            case '/':
+                if( value !== 0) setValue(0);
+                break;
+            case '/services':
+                if( value !== 1){
+                    setValue(1);
+                    setSelectedIndex(0);
+                }
+                break;
+            case '/customsoftware':
+                if( value !== 1){
+                    setValue(1);
+                    setSelectedIndex(1);
+                }
+                break;
+            case '/mobileapps':
+                if( value !== 1){
+                    setValue(1);
+                    setSelectedIndex(2);
+                }
+                break;
+            case '/websites':
+                if( value !== 1){
+                    setValue(1);
+                    setSelectedIndex(3);
+                }
+                break;
+            case '/revolution':
+                if( value !== 2) setValue(2);
+                break;
+            case '/aboutus':
+                if( value !== 3) setValue(3);
+                break;
+            case '/contact':
+                if( value !== 4) setValue(4);
+                break;
+            case '/estimate':
+                if( value !== 5) setValue(5);
+                break;
+            default:
+                break;
+        }
+    }, [value])
 
     return(
         <React.Fragment>
@@ -87,7 +159,15 @@ export default function Header(){
                         </Button>
                         <Tabs value={value} className={classes.tabContainer} onChange={handlechange} indicatorColor="primary">
                             <Tab className={classes.tab} label="Home" component={Link} to="/"/>
-                            <Tab className={classes.tab} label="Services" component={Link} to="/services"/>
+                            <Tab 
+                                aria-owns={anchorEl ? "services-menu" : undefined}
+                                aria-haspopup={anchorEl ? 'true' : undefined}
+                                onMouseOver={(event) => handleClick(event)}
+                                className={classes.tab} 
+                                label="Services" 
+                                component={Link} 
+                                to="/services"
+                            />
                             <Tab className={classes.tab} label="The revolution" component={Link} to="/revolution"/>
                             <Tab className={classes.tab} label="About us" component={Link} to="/aboutus"/>
                             <Tab className={classes.tab} label="Contact us" component={Link} to="/contact"/>
@@ -95,6 +175,34 @@ export default function Header(){
                         <Button variant="contained" color="secondary" className={classes.button}>
                             Free Estimate
                         </Button>
+                        <Menu 
+                            id="services-menu" 
+                            anchorEl={anchorEl} 
+                            open={open} 
+                            onClose={handleClose}
+                            MenuListProps={{onMouseLeave: handleClose}}
+                            classes={{paper: classes.menu}}
+                            elevation={0}
+                        >
+                            {
+                                menuOptions.map((option, index) => (
+                                    <MenuItem
+                                        key={index}
+                                        component={Link}
+                                        to={`${option.link}`}
+                                        classes={{root: classes.menuItem}}
+                                        onClick={(event, index) => {
+                                            handleMenuItemClick(event, index);
+                                            setValue(1);
+                                            handleClose();
+                                        }}
+                                        selected={index === selectedIndex && value === 1}
+                                    >
+                                        {option.name}
+                                    </MenuItem>
+                                ))
+                            }
+                        </Menu>
                     </Toolbar>
                 </AppBar>
             </ElevationScroll>
